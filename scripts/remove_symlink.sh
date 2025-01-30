ada4979983747dc69327fc9b5fa14528b9620e65
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-01-30 10:16:28 +0545

    Update remove_symlink.sh

--- scripts/remove_symlink.sh
+++ scripts/remove_symlink.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# Symlink removal function
 function remove_symlink() {
     rm -rf "$1"
     echo "Symlink $1 is removed"
@@ -13,26 +16,26 @@ remove_symlink ~/.gitconfig
 remove_symlink ~/.gitignore_global
 
 # Remove symlinks for Vim
-remove_symlink ~/.vimrc
-remove_symlink ~/.vim/plugins.vim
+remove_symlink ~/.config/vim/vimrc
+remove_symlink ~/.config/vim/plugin.vim
 
 # Remove symlinks for Neovim
 remove_symlink ~/.config/nvim/init.vim
 
 echo ""
 
-# Ensure all necessary directories is removed after all the symlinks are removed
-rm -rf ~/.vim
-rm -rf ~/.config
+# Ensure all necessary directories are removed after all the symlinks are removed
+rm -rf ~/.config/vim
+rm -rf ~/.config/nvim
 
-if [ ! -d ~/.vim ]
+if [ ! -d ~/.config/vim ]
 then
-	dir_name="~/.vim"
-	echo "Directory $dir_name is removed"
+    dir_name="~/.config/vim"
+    echo "Directory $dir_name is removed"
 fi
 
 if [ ! -d ~/.config/nvim ]
 then
-	dir_name="~/.config/nvim"
-	echo "Directory $dir_name is removed"
+    dir_name="~/.config/nvim"
+    echo "Directory $dir_name is removed"
 fi
