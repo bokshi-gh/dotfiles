ee5bc2b5f1302ff605c9733bef068a1991ce6093
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-04-14 13:02:06 +0545

    Update remove_symlink.sh

--- scripts/remove_symlink.sh
+++ scripts/remove_symlink.sh
@@ -6,6 +6,9 @@ function remove_symlink() {
     echo "Symlink $1 is removed"
 }
 
+# Remove symlinks for Bash
+remove_symlink ~/.bashrc
+
 # Remove symlinks for Git
 remove_symlink ~/.gitconfig
 remove_symlink ~/.gitignore_global
@@ -13,14 +16,18 @@ remove_symlink ~/.gitignore_global
 # Remove symlinks for Vim
 remove_symlink ~/.vimrc
 
-
 rm -rf ~/.config
-
 if [ ! -d ~/.config ]
 then
     dir_name="~/.config"
     echo "Directory $dir_name is removed"
 fi
 
-remove_symlink ~/.ssh/config
-remove_symlink ~/.ssh/id_rsa_github
+echo ""
+
+rm -rf ~/.ssh
+if [ ! -d ~/.ssh ]
+then
+    dir_name="~/.config"
+    echo "Directory $dir_name is removed"
+fi
