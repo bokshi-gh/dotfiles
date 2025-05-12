7ff50a3590a082ab4708e9bb2fc3a1e8d33a983b
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-05-12 11:58:54 +0545

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,5 @@
 vim.cmd("set number")
-
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
