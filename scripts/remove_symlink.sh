2aedaaa8e85849747950b14b404e4e185a19d393
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-08-04 00:12:27 +0545

    Update remove_symlink.sh

--- scripts/remove_symlink.sh
+++ scripts/remove_symlink.sh
@@ -24,19 +24,20 @@ remove_symlink() {
     fi
 }
 
-# Run cleanup script
-if [ -f ~/.config/nvim/scripts/cleanup.sh ]; then
-    echo -e "${GREEN}[RUNNING NEOVIM CLEANUP SCRIPT]${NC}"
-    bash ~/.config/nvim/scripts/cleanup.sh
-else
-    echo -e "${RED}NEOVIM CLEANUP SCRIPT NOT FOUND:${NC} ~/.config/nvim/scripts/cleanup.sh"
-fi
-
 remove_symlink ~/.gitconfig
 remove_symlink ~/.gitignore_global
 remove_symlink ~/.vimrc
 remove_symlink ~/.config/nvim
 remove_symlink ~/.ssh/config
 remove_symlink ~/.ssh/id_rsa_github
+
+# Run cleanup script
+if [ -f ~/.config/nvim/scripts/cleanup.sh ]; then
+    echo ""
+    echo -e "${GREEN}[RUNNING NEOVIM CLEANUP SCRIPT]${NC}"
+    bash ~/.config/nvim/scripts/cleanup.sh
+else
+    echo -e "${RED}NEOVIM CLEANUP SCRIPT NOT FOUND:${NC} ~/.config/nvim/scripts/cleanup.sh"
+fi
 
 echo -e "${GREEN}[ALL DONE! SYMLINKS REMOVAL COMPLETE]${NC}"
