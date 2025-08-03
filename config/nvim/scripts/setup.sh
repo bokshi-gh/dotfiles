3121f4fd3eb02790c2727bddcce9be85779ffe29
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-08-04 00:44:19 +0545

    Update setup.sh

--- config/nvim/scripts/setup.sh
+++ config/nvim/scripts/setup.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
+
+echo ""
+echo -e "${GREEN}[RUNNING NEOVIM SETUP SCRIPT]${NC}"
 
 echo -e "${YELLOW}=== UPDATING SYSTEM ===${NC}"
 sudo apt update && sudo apt upgrade -y
