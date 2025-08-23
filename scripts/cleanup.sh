8e2d10c25b817b0d84bc754bbc949f1e03732ea8
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-08-24 05:10:27 +0545

    Update and rename remove_symlink.sh to cleanup.sh

---
+++ scripts/cleanup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# ANSI color codes
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}[DELETING DOTFILES FILES/FOLDERS]${NC}"
+
+delete_file_or_dir() {
+    local target=$1
+    if [ -e "$target" ]; then
+        rm -rf "$target"
+        echo -e "${GREEN}Deleted:${NC} $target"
+    else
+        echo -e "${RED}Skipped:${NC} $target does not exist"
+    fi
+}
+
+# List of files/folders to delete
+targets=(
+    ~/.gitconfig
+    ~/.vimrc
+    ~/.config/nvim
+    ~/.ssh/config
+    ~/.ssh/id_rsa_github
+)
+
+for t in "${targets[@]}"; do
+    delete_file_or_dir "$t"
+done
