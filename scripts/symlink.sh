423de03487449ab227ac005ae547b508c498529a
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-08-24 03:10:59 +0545

    Update symlink.sh

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -4,11 +4,6 @@ GREEN='\033[1;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-# Remove older symlinks
-if [ -f ~/.dotfiles/scripts/remove_symlink.sh ]; then
-    bash ~/.dotfiles/scripts/remove_symlink.sh
-fi
-
 symlink() {
     local src=$1
     local dest=$2
@@ -25,7 +20,6 @@ symlink() {
 mkdir -p ~/.config
 mkdir -p ~/.ssh
 
-echo ""
 echo -e "${GREEN}[SETTING UP SYMLINKS]${NC}"
 
 symlink ~/.dotfiles/git/.gitconfig ~/.gitconfig
@@ -35,10 +29,3 @@ symlink ~/.dotfiles/ssh/config ~/.ssh/config
 symlink ~/.dotfiles/ssh/id_rsa_github ~/.ssh/id_rsa_github
 
 chmod 600 ~/.dotfiles/ssh/id_rsa_github
-
-# Run setup script for Neovim
-if [ -f ~/.dotfiles/config/nvim/scripts/setup.sh ]; then
-    bash ~/.dotfiles/config/nvim/scripts/setup.sh
-else
-    echo -e "${RED}NEOVIM SETUP SCRIPT NOT FOUND:${NC} ~/.dotfiles/config/nvim/scripts/setup.sh"
-fi
