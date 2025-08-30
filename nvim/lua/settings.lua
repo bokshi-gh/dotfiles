8325a0e88312ef88e645dec82790de1c14790cfa
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-08-30 22:38:26 +0545

    Update settings.lua

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,1 +1,10 @@
-vim.o.relativenumber = true
+vim.o.background = "dark"
+
+-- Remove ~ at end of buffer
+vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "NONE" })
+
+-- Make normal buffer and NeoTree transparent / use terminal background
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE" })
