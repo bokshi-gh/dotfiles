ed7f252af52619d9899377dbc3180eb1188ff125
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-08-30 22:52:45 +0545

    Update mason-lspconfig.lua

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -9,25 +9,29 @@ return {
     local lspconfig = require("lspconfig")
     local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    local function map(mode, lhs, rhs, desc, bufnr)
-      vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, noremap = true, silent = true, desc = desc })
+    local function map_buf(bufnr)
+      local function nmap(lhs, rhs, desc)
+        vim.keymap.set("n", lhs, rhs, { buffer = bufnr, noremap = true, silent = true, desc = desc })
+      end
+
+      nmap("K", vim.lsp.buf.hover, "Hover Docs")
+      nmap("gd", vim.lsp.buf.definition, "Go to Definition")
+      nmap("gD", vim.lsp.buf.declaration, "Go to Declaration")
+      nmap("gi", vim.lsp.buf.implementation, "Go to Implementation")
+      nmap("gr", vim.lsp.buf.references, "List References")
+      nmap("<leader>rn", vim.lsp.buf.rename, "Rename Symbol")
+      nmap("<leader>ca", vim.lsp.buf.code_action, "Code Action")
+      nmap("<leader>f", function() vim.lsp.buf.format({ async = true }) end, "Format File")
+      nmap("[d", vim.diagnostic.goto_prev, "Previous Diagnostic")
+      nmap("]d", vim.diagnostic.goto_next, "Next Diagnostic")
+      nmap("<leader>e", vim.diagnostic.open_float, "Show Diagnostic")
     end
--- Set dark background
+
     local on_attach = function(_, bufnr)
-      map("n", "K", vim.lsp.buf.hover, "Hover Docs", bufnr)
-      map("n", "gd", vim.lsp.buf.definition, "Go to Definition", bufnr)
-      map("n", "gD", vim.lsp.buf.declaration, "Go to Declaration", bufnr)
-      map("n", "gi", vim.lsp.buf.implementation, "Go to Implementation", bufnr)
-      map("n", "gr", vim.lsp.buf.references, "List References", bufnr)
-      map("n", "<leader>rn", vim.lsp.buf.rename, "Rename Symbol", bufnr)
-      map("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action", bufnr)
-      map("n", "<leader>f", function()
-        vim.lsp.buf.format({ async = true })
-      end, "Format File", bufnr)
-      map("n", "[d", vim.diagnostic.goto_prev, "Previous Diagnostic", bufnr)
-      map("n", "]d", vim.diagnostic.goto_next, "Next Diagnostic", bufnr)
-      map("n", "<leader>e", vim.diagnostic.open_float, "Show Diagnostic", bufnr)
+      map_buf(bufnr)
     end
+
+    require("mason").setup()
 
     require("mason-lspconfig").setup({
       ensure_installed = {
@@ -37,28 +41,26 @@ return {
       automatic_installation = true,
     })
 
-    require("mason-lspconfig").setup_handlers({
-      -- default setup for all servers
-      function(server_name)
-        lspconfig[server_name].setup({
-          capabilities = cmp_capabilities,
-          on_attach = on_attach,
-        })
-      end,
-      -- overrides
-      ["lua_ls"] = function()
-        lspconfig.lua_ls.setup({
-          capabilities = cmp_capabilities,
-          on_attach = on_attach,
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { "vim" },
-              },
-            },
-          },
-        })
-      end,
-    })
+    local servers = {
+      clangd = {},
+      jdtls = {},
+      lua_ls = {
+        settings = {
+          Lua = { diagnostics = { globals = { "vim" } } }
+        }
+      },
+      gopls = {},
+      pyright = {},
+      rust_analyzer = {},
+      ts_ls = {},
+      html = {},
+      cssls = {},
+    }
+
+    for name, config in pairs(servers) do
+      config.capabilities = cmp_capabilities
+      config.on_attach = on_attach
+      lspconfig[name].setup(config)
+    end
   end,
 }
