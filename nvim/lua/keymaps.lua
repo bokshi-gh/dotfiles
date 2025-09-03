00c86b4d9b402c702d0a778d450c960a17f0bc43
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-09-03 11:09:55 +0545

    Update keymaps.lua

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -6,6 +6,9 @@ vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = "Quit file" })
 vim.keymap.set('n', '<leader>qd', ':q!<CR>', { desc = "Force quit without saving" })
 vim.keymap.set('n', '<leader>wq', ':wq<CR>', { desc = "Save and quit" })
 
+vim.keymap.set('n', '<leader>tn', ':tabnew<CR>', { desc = "Open tab new" })
+vim.keymap.set('n', '<leader>tnf', ':tabnew ', { desc = "Prepare :tabnew for filename" })
+
 vim.keymap.set('n', '<leader>rc', ':!gcc % -o %:r && ./%:r<CR>', { desc = "Compile & run C file" })
 vim.keymap.set('n', '<leader>rcpp', ':!g++ % -o %:r && ./%:r<CR>', { desc = "Compile & run C++ file" })
 vim.keymap.set('n', '<leader>rrs', ':!cargo run<CR>', { desc = "Run Rust project with Cargo" })
