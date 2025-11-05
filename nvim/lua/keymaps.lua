75d51d16d1b7b366d7ca19d92453524f506ea5c0
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-05 17:15:44 +0545

    remove some keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,13 +1,6 @@
 vim.g.mapleader = " "
 
 vim.keymap.set('n', '<leader>t', ':terminal<CR>', { desc = "Open terminal" })
-vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = "Save file" })
-vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = "Quit file" })
-vim.keymap.set('n', '<leader>qd', ':q!<CR>', { desc = "Force quit without saving" })
-vim.keymap.set('n', '<leader>wq', ':wq<CR>', { desc = "Save and quit" })
-
-vim.keymap.set('n', '<leader>tn', ':tabnew<CR>', { desc = "Open tab new" })
-vim.keymap.set('n', '<leader>tnf', ':tabnew ', { desc = "Prepare :tabnew for filename" })
 
 vim.keymap.set('n', '<leader>rc', ':!gcc % -o %:r && ./%:r<CR>', { desc = "Compile & run C file" })
 vim.keymap.set('n', '<leader>rcpp', ':!g++ % -o %:r && ./%:r<CR>', { desc = "Compile & run C++ file" })
@@ -20,7 +13,3 @@ vim.keymap.set('n', '<leader>rjs', ':!node %<CR>', { desc = "Run Node.js file" }
 vim.keymap.set('n', '<leader>tn', function()
   vim.wo.number = not vim.wo.number
 end, { desc = "Toggle line numbers" })
-
-vim.keymap.set('n', '<leader>trn', function()
-  vim.wo.relativenumber = not vim.wo.relativenumber
-end, { desc = "Toggle relative line numbers" })
