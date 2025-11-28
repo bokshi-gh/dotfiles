ef63cbbaa0e820da29a0a964084cb2d46f5d5180
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-28 21:20:23 +0545

    auto indent to false

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -2,4 +2,10 @@ vim.o.background = "dark"
 vim.cmd([[colorscheme default]])
 vim.o.fillchars = "eob: "
 
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.smartindent = true
+vim.o.autoindent = true
 
