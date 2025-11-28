7790d425601d537b7278630343c949414e898912
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-28 21:14:35 +0545

    auto indent to false

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -8,7 +8,7 @@ return {
           ensure_installed = {"asm", "bash", "c", "c_sharp", "cmake", "cpp", "css", "csv", "dockerfile", "gitignore", "go", "git_config", "html", "java", "javascript", "json", "kotlin", "llvm", "lua", "make", "markdown", "nasm", "nginx", "python", "php", "proto", "rust", "sql", "ssh_config", "tmux", "toml", "tsx", "typescript", "vim", "xml", "yaml"},
           sync_install = false,
           highlight = { enable = true },
-          indent = { enable = true },  
+          indent = { enable = false },  
         })
     end
 }
