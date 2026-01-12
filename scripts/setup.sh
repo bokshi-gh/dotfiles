98a48b6488d38aedc4e86cbbf5f60d9c52a1ba59
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-01-12 16:08:59 +0545

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,6 +7,8 @@ GREEN='\033[1;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
+sudo ~/.dotfiles/cleanup.sh
+
 copy_file_or_dir() {
     local src=$1
     local dest=$2
@@ -25,14 +27,12 @@ copy_file_or_dir() {
     echo -e "${GREEN}Copied:${NC} $src → $dest"
 }
 
-mkdir -p ~/.config
 mkdir -p ~/.ssh
 
 echo -e "${GREEN}[SETTING UP CONFIGURATION FILES AND FOLDERS]${NC}"
 
 copy_file_or_dir ~/.dotfiles/git/.gitconfig ~/.gitconfig
 copy_file_or_dir ~/.dotfiles/vim/.vimrc ~/.vimrc
-copy_file_or_dir ~/.dotfiles/nvim ~/.config/nvim
 copy_file_or_dir ~/.dotfiles/ssh/config ~/.ssh/config
 copy_file_or_dir ~/.dotfiles/ssh/id_rsa_github ~/.ssh/id_rsa_github
 chmod 600 ~/.ssh/id_rsa_github
