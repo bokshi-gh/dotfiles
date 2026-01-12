5987196d4645ccab369c2ab7e2c9955cc0ad2b0a
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-01-12 20:49:27 +0545

    Update cleanup.sh

--- nvchad/scripts/cleanup.sh
+++ nvchad/scripts/cleanup.sh
@@ -1,6 +1,13 @@
-rm -rf ~/.config/nvim
-rm -rf ~/.local/state/nvim
-rm -rf ~/.local/share/nvim
+rm -rf ~/.config/nvim \
+       ~/.local/state/nvim \
+       ~/.local/share/nvim
 
-sudo pacman -Rns $(pacman -Qdtq)
+orphans=$(pacman -Qdtq)
+
+if [ -n "$orphans" ]; then
+    sudo pacman -Rns $orphans
+else
+    echo "No orphan packages found."
+fi
+
 sudo pacman -Sc
