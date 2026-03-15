6ace340198055952eed0b91a4952683be58cc650
Rajesh Thapa <169505071+bokshi-gh@users.noreply.github.com>
2026-03-15 18:37:36 +0545

    Update setup.sh

--- nvchad/scripts/setup.sh
+++ nvchad/scripts/setup.sh
@@ -10,7 +10,10 @@ else
 fi
 
 sudo pacman -Syu --noconfirm
-sudo pacman -S --needed --noconfirm git neovim ripgrep gcc
+sudo pacman -S --needed --noconfirm git
+sudo pacman -S --needed --noconfirm neovim
+sudo pacman -S --needed --noconfirm ripgrep
+sudo pacman -S --needed --noconfirm base-devel gcc make
 
 git clone https://github.com/NvChad/starter ~/.config/nvim && \
 nvim --headless "+Lazy sync" "+MasonInstallAll" "+TSInstallAll" "+qa" && \
