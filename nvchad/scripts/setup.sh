899fab99ca10f63a042a14a714be4debd19186ef
Rajesh Thapa <169505071+bokshi-gh@users.noreply.github.com>
2026-03-15 18:12:18 +0545

    Update setup.sh

--- nvchad/scripts/setup.sh
+++ nvchad/scripts/setup.sh
@@ -1,3 +1,9 @@
+if [ -f "$HOME/.dotfiles/nvchad/scripts/cleanup.sh" ]; then
+    bash "$HOME/.dotfiles/nvchad/scripts/cleanup.sh"
+else
+    echo -e "${RED}Cleanup script not found${NC}"
+fi
+
 sudo pacman -Syu --noconfirm
 sudo pacman -S --needed --noconfirm git neovim ripgrep gcc
 
