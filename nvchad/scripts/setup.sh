49d9582a3fb315c6b6a621a385e65654562a6fe8
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-08-09 10:16:38 +0545

    Update setup.sh

--- nvchad/scripts/setup.sh
+++ nvchad/scripts/setup.sh
@@ -12,6 +12,7 @@ fi
 sudo pacman -Syu --noconfirm
 sudo pacman -S --needed --noconfirm git
 sudo pacman -S --needed --noconfirm neovim
+sudo pacman -S --needed --noconfirm tree-sitter-cli
 sudo pacman -S --needed --noconfirm ripgrep
 sudo pacman -S --needed --noconfirm base-devel gcc make
 
