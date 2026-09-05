2cb8c4216388a4d301e86b76e3cb877b7bc6308d
Bokshi <itsbokshi@gmail.com>
2026-09-05 15:17:26 +0545

    Update setup-nvchad.sh

--- scripts/setup-nvchad.sh
+++ scripts/setup-nvchad.sh
@@ -7,7 +7,6 @@ sudo pacman -S --needed --noconfirm tree-sitter-cli
 sudo pacman -S --needed --noconfirm ripgrep
 sudo pacman -S --needed --noconfirm base-devel gcc make
 
-git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
-
-# After running this setup, check:
-# ../docs/nvchad.md
+git clone https://github.com/NvChad/starter ~/.config/nvim &&
+xdg-open "https://github.com/bokshi-gh/dotfiles/blob/main/docs/nvchad.md" &&
+nvim
