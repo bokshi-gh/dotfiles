8b26f756b78fd174ed307caca745b320aa955869
Bokshi <itsbokshi@gmail.com>
2026-09-05 19:49:24 +0545

    Update setup-nvchad.sh

--- scripts/setup-nvchad.sh
+++ scripts/setup-nvchad.sh
@@ -1,13 +1,50 @@
 #!/bin/bash
 
 sudo pacman -Syu --noconfirm
-sudo pacman -S --needed --noconfirm git
-sudo pacman -S --needed --noconfirm neovim
-sudo pacman -S --needed --noconfirm tree-sitter-cli
-sudo pacman -S --needed --noconfirm ripgrep
-sudo pacman -S --needed --noconfirm base-devel gcc make
 
-git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+# Basic tools
+sudo pacman -S --needed --noconfirm \
+    git \
+    neovim \
+    tree-sitter-cli \
+    ripgrep \
+    base-devel
+
+# C/C++
+sudo pacman -S --needed --noconfirm \
+    gcc \
+    gdb \
+    clang \
+    cmake
+
+# Assembly
+sudo pacman -S --needed --noconfirm \
+    nasm
+
+# Rust
+sudo pacman -S --needed --noconfirm \
+    rustup
+
+rustup default stable
+
+# Java
+sudo pacman -S --needed --noconfirm \
+    jdk-openjdk
+
+# JavaScript / TypeScript
+sudo pacman -S --needed --noconfirm \
+    nodejs \
+    npm
+
+# Python
+sudo pacman -S --needed --noconfirm \
+    python \
+    python-pip
+
+# NvChad
+git clone https://github.com/NvChad/starter ~/.config/nvim
+
+nvim
 
 # After running this setup, check:
 # ../docs/nvchad.md
