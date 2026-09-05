23db5ff3fd076c960566d15e6d0fb6445e5893d1
Bokshi <itsbokshi@gmail.com>
2026-09-05 23:43:34 +0545

    Update setup-nvchad.sh

--- scripts/setup-nvchad.sh
+++ scripts/setup-nvchad.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 sudo pacman -Syu --noconfirm
 sudo pacman -S --needed --noconfirm git
 sudo pacman -S --needed --noconfirm neovim
@@ -7,6 +9,7 @@ sudo pacman -S --needed --noconfirm tree-sitter-cli
 sudo pacman -S --needed --noconfirm ripgrep
 sudo pacman -S --needed --noconfirm base-devel gcc make
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
 
 # After running this setup, check:
